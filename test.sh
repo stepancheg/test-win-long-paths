8320716d1c62aabbd39bb1396a320bea90e0099e
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-12-10 18:18:45 +0000

    2

--- test.sh
+++ test.sh
@@ -3,10 +3,15 @@
 set -ex
 
 d=foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux
+
 mkdir -p "$d"
 
 git clone https://github.com/tokio-rs/bytes
-mv bytes d
+
+mv bytes "$d"
+
+test -e "$d/Cargo.toml"
+
 cargo build --manifest-path "$d/Cargo.toml"
 
 
