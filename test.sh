2155ceff7f4ef52f27e9a0368bbaf52c3e0ac03a
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-12-10 18:12:23 +0000

    !!!

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -ex
+
+d=foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux
+mkdir -p "$d"
+cd "$d"
+
+pwd
+
+
+# vim: set ts=4 sw=4 et:
