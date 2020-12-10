59237c68fbdd8d3e37dcfccbb540342e6dfe6394
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-12-10 18:21:57 +0000

    4

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-d=foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux
+d="foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux/foo/bar/baz/qux/quux"
 
 mkdir -p "$d"
 
@@ -10,9 +10,11 @@ git clone https://github.com/tokio-rs/bytes
 
 mv bytes "$d"
 
-ls "$d"
+ls "$d/bytes"
 
-cargo build --manifest-path "$d/Cargo.toml"
+test -e "$d/bytes/Cargo.toml"
+
+cargo build --manifest-path "$d/bytes/Cargo.toml"
 
 
 # vim: set ts=4 sw=4 et:
